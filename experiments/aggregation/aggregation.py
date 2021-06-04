e5faf2246e1c5549771c38532187741cc903032e
Valhrona <sohail.elsabrouty202@gmail.com>
2021-06-04 16:38:19 +0200

    First attempt

--- experiments/aggregation/aggregation.py
+++ experiments/aggregation/aggregation.py
@@ -6,4 +6,51 @@ from simulation.swarm import Swarm
 
 class Aggregations(Swarm):
     """ """
-    pass
+    def __init__(self, screen_size) -> None:
+        super(Aggregations, self).__init__(screen_size)
+
+    def initialize(self, num_agents: int) -> None:
+
+        "location of the obstacle and the aggregation site"
+        loc = config["base"]["object_location"]
+
+        obstacle_scale = [500, 500]
+        obstacle_filename = ( "experiments/flocking/images/redd.png" )
+
+        "Add containing object (outer edge)"
+        self.objects.add_object(
+            file=obstacle_filename, pos=loc, scale=obstacle_scale, obj_type="obstacle"
+        )
+
+        aggregation_scale = [100,100]
+        aggregation_filename = ("experiments/aggregation/images/greyc1.png")
+
+        "Add aggregation site"
+        self.objects.add_object(
+            file=aggregation_filename, pos=loc, scale=aggregation_scale, obj_type="site"
+        )
+        min_x, max_x = area(loc[0], loc[0])
+        min_y, max_y = area(loc[1], loc[1])
+
+        # add agents to the environment
+        for index, agent in enumerate(range(num_agents)):
+            coordinates = generate_coordinates(self.screen)
+
+            # # if obstacles present re-estimate the coordinates
+            # if config["base"]["obstacles"]:
+            #     if config["flock"]["outside"]:
+            #         while (
+            #                 max_x >= coordinates[0] >= min_x
+            #                 and max_y >= coordinates[1] >= min_y
+            #         ):
+            #             coordinates = generate_coordinates(self.screen)
+            #     else:
+            #         while (
+            #                 coordinates[0] >= max_x
+            #                 or coordinates[0] <= min_x
+            #                 or coordinates[1] >= max_y
+            #                 or coordinates[1] <= min_y
+            #         ):
+            #             coordinates = generate_coordinates(self.screen)
+
+            self.add_agent(Cockroach(pos=np.array(coordinates), v=None, state="wandering", index=index))
--- experiments/aggregation/cockroach.py
+++ experiments/aggregation/cockroach.py
@@ -1,3 +1,4 @@
+import numpy as np
 from experiments.aggregation.config import config
 from simulation.agent import Agent
 from simulation.utils import *
@@ -5,4 +6,30 @@ from simulation.utils import *
 
 class Cockroach(Agent):
     """ """
-    pass
+    def __init__(
+            self, pos, v, state, index: int, image: str = "experiments/aggregation/images/ant.png"
+    ) -> None:
+        super(Cockroach, self).__init__(
+            pos,
+            v,
+            image,
+            max_speed=config["agent"]["max_speed"],
+            min_speed=config["agent"]["min_speed"],
+            mass=config["agent"]["mass"],
+            width=config["agent"]["width"],
+            height=config["agent"]["height"],
+            dT=config["agent"]["dt"],
+            index=index
+        )
+        self.state = state
+
+    def change_state(self,new_state):
+        self.state = new_state
+
+    def site_behavior(self):
+        if self.pos == #check if cockroach is in the site which is round
+            if self.state == "wandering":
+                if np.random.choice([True,False],p=[config["cockroach"]["joining_probability"],1-config["cockroach"]["joining_probability"]]):
+                    self.change_state("joining")
+
+    def update_actions(self):
