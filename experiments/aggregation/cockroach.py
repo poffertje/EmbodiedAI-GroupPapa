4866a7605648150d5c239b5eb59b5b2adcf93c1e
Valhrona <sohail.elsabrouty202@gmail.com>
2021-06-06 17:03:03 +0200

    -fix leaving probability and joining probability

--- experiments/aggregation/cockroach.py
+++ experiments/aggregation/cockroach.py
@@ -1,8 +1,8 @@
 import numpy as np
+import time
 from experiments.aggregation.config import config
 from simulation.agent import Agent
 from simulation.utils import *
-from datetime import datetime
 
 
 class Cockroach(Agent):
@@ -24,7 +24,8 @@ class Cockroach(Agent):
         )
         self.state = state
         self.flock = flock
-        self.timer = 0
+        self.time = 0
+        self.counter = 0
 
     def change_state(self,new_state):
         self.state = new_state
@@ -33,11 +34,24 @@ class Cockroach(Agent):
         if self.pos[0] in range(area(500,300)) and self.pos[1] in range(area(500,300)):
             if self.state == "wandering":
                 nr_neighbours = len(self.flock.find_neighbors(self, config["cockroach"]["radius_view"]))
-                probability = config["base"]["n_agents"]/nr_neighbours
-                print(probability)
+                probability = nr_neighbours/config["base"]["n_agents"]
                 if np.random.choice([True,False],p=[probability,1-probability]):
                     self.change_state("joining")
-                    self.timer = datetime.now().time()
+                    self.time = time.time()
             elif self.state == "joining":
+                if time.time()-self.time == 2.00:
+                    self.state = "still"
+            elif self.state == "still":
+                self.counter+=1
+                if self.counter % 5 == 0:
+                    nr_neighbours = len(self.flock.find_neighbors(self, config["cockroach"]["radius_view"]))
+                    probability =  (config["base"]["n_agents"]-nr_neighbours)/config["base"]["n_agents"]
+                    if np.random.choice([True, False], p=[probability, 1 - probability]):
+                        self.state = "leaving"
+                        self.time = time.time()
+        elif not(self.pos[0] in range(area(500,300)) and self.pos[1] in range(area(500,300))):
+            if self.state == "leaving":
+                if time.time()-self.time == 6.00:
+                    self.state == "wandering"
 
-    # def update_actions(self):
+    def update_actions(self):
